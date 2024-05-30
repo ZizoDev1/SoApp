de3acfadaee4e29077c59afbb81acd65149393f7
Adham <iamnotadham@gmail.com>
2024-05-30 16:47:07 +0300

    End of Signup

--- SoApp/ncrpt.py
+++ SoApp/ncrpt.py
@@ -1,30 +1,17 @@
 from dictionary import *
-from signup import *
+
+
 def encrypt(string):
     string = string.lower()
-    userPassword = string.translate(
+    string = string.translate(
         str.maketrans({"a": A, "b": B, "c": C, "d": D, "e": E, "f": F, "g": G, "h": h, "i": i, "j": J, "k": K, "l": L,
                     "m": M, "n": N, "o": o, "p": P, "q": Q, "r": R, "s": S, "t": T, "u": U, "v": V, "w": W, "x": X,
-                    "y": Y, "z": Z, " ": space
-                    }))
-    print(userPassword)
+                    "y": Y, "z": Z, " ": space}))
 
 
 def decrypt(string):
-    userPassword = string
-    userPassword = userPassword.replace(A, "a").replace(B, "b").replace(C, "c").replace(D, "d").replace(E, "e").replace(
+    string = string.replace(A, "a").replace(B, "b").replace(C, "c").replace(D, "d").replace(E, "e").replace(
         F, "f").replace(G, "g").replace(h, "h").replace(i, "i").replace(J, "j").replace(K, "k").replace(L, "l").replace(
         M, "m").replace(N, "n").replace(o, "o").replace(P, "p").replace(Q, "q").replace(R, "r").replace(S, "s").replace(
         T, "t").replace(U, "u").replace(V, "v").replace(W, "w").replace(X, "x").replace(Y, "y").replace(Z, "z").replace(
         space, " ")
-
-
-
-# en = encrypt
-"""
-en_first_name = encrypt(first_name)
-en_last_name = encrypt(last_name)
-en_pass_word = encrypt(pass_word)
-en_email = encrypt(email)
-en_username = encrypt(user_name)
-"""
--- SoApp/signup.py
+++ SoApp/signup.py
@@ -1,4 +1,50 @@
 from ncrpt import *
+from getpass import getpass
+
+
+fullName = ""
+firstName = ""
+lastName = ""
+userEmail = ""
+password = ""
+rePassword = ""
+
 print("------------Sign up------------")
+print("  Please enter your information  ")
+
 
 def signup():
+    global fullName, firstName, lastName, userEmail, password, rePassword
+
+    # check if user entered full name
+    while True:
+        fullName = input("Enter your full name \n --> ")
+        if " " in fullName:
+            firstName, lastName = fullName.split(" ", 1)
+            break
+        else:
+            print("Please enter your full name with a space between first and last names.")
+
+    userEmail = input("Enter your email address \n --> ")
+    password = getpass("Enter your password \n --> ")
+    rePassword = getpass("Re-enter your password \n -->")
+
+# check if passwords rematch
+
+    while password != rePassword:
+        print("Passwords do not match")
+        password = getpass("Enter your password \n --> ")
+        rePassword = getpass("Re-enter your password \n -->")
+
+    print("Thank you for signing up")
+
+
+signup()
+
+
+encrypt(fullName)
+encrypt(firstName)
+encrypt(lastName)
+encrypt(userEmail)
+encrypt(password)
+
