329870d6e450bcdb14b7a2fdb98d86854d278ae2
Adham <iamnotadham@gmail.com>
2024-05-30 23:49:58 +0300

    Password requirements

--- SoApp/signup.py
+++ SoApp/signup.py
@@ -1,5 +1,4 @@
 from ncrpt import *
-from getpass import getpass
 from hashing import *
 
 
@@ -43,17 +42,26 @@ def signup():
             print("That's not a valid email address")
             print("----------------------")
 
-    password = getpass("Enter your password \n requirements (8 characters, numbers, and special characters) \n --> ")
-    rePassword = getpass("Re-enter your password \n -->")
+    def ValidPass():
+        while True:
+            password = input(
+                "Enter your password (must be at least 8 characters long and include at least one number) \n --> ")
 
-# check if passwords rematch
+            # Check if password meets requirements
+            if len(password) < 8:
+                print("Password must be at least 8 characters long.")
+            elif not any(char.isdigit() for char in password):
+                print("Password must include at least one number.")
+            else:
+                # Ask user to re-enter password for confirmation
+                rePassword = input("Re-enter your password for confirmation \n --> ")
+                if password == rePassword:
+                    return password
+                elif password != rePassword:
+                    print("Passwords do not match. Please try again.")
+                    ValidPass()
 
-    while password != rePassword:
-        print("Passwords do not match")
-        password = getpass("Enter your password \n --> ",)
-        print("----------------------")
-        password = getpass("Enter your password \n --> ")
-        rePassword = getpass("Re-enter your password \n -->")
+    password = ValidPass()
 
 
 signup()
@@ -66,7 +74,6 @@ userName = encrypt(userName)
 userEmail = encrypt(userEmail)
 password = encrypt(password)
 
-print(fullName)
 try:
     # Call the hashing function and store the hashed password
     password = HashingInfo(password)
@@ -75,3 +82,6 @@ try:
     print(f"Successfully signed up")
 except Exception as e:
     print(f"An error occurred: {e}")
+    signup()
+
+
