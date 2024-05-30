5435ecf41408ec6779e35065960e6e27e4b9f987
Adham <iamnotadham@gmail.com>
2024-05-30 12:40:36 +0300

    n/d cryption done

---
+++ SoApp/ncrpt.py
@@ -0,0 +1,30 @@
+from dictionary import *
+def encrypt(string):
+    string = string.lower()
+    userPassword = string.translate(
+        str.maketrans({"a": A, "b": B, "c": C, "d": D, "e": E, "f": F, "g": G, "h": h, "i": i, "j": J, "k": K, "l": L,
+                       "m": M, "n": N, "o": o, "p": P, "q": Q, "r": R, "s": S, "t": T, "u": U, "v": V, "w": W, "x": X,
+                       "y": Y, "z": Z, " ": space}))
+    print(userPassword)
+
+
+def decrypt(string):
+    userPassword = string
+    userPassword = userPassword.replace(A, "a").replace(B, "b").replace(C, "c").replace(D, "d").replace(E, "e").replace(
+        F, "f").replace(G, "g").replace(h, "h").replace(i, "i").replace(J, "j").replace(K, "k").replace(L, "l").replace(
+        M, "m").replace(N, "n").replace(o, "o").replace(P, "p").replace(Q, "q").replace(R, "r").replace(S, "s").replace(
+        T, "t").replace(U, "u").replace(V, "v").replace(W, "w").replace(X, "x").replace(Y, "y").replace(Z, "z").replace(
+        space, " ")
+
+    print(userPassword)
+
+
+userChoice = input("Do you want to encrypt or decrypt (e,d) \n --> ")
+userInput = input("Enter text you want to encrypt or decrypt \n --> ")
+
+if userChoice.lower() == "e":
+    encrypt(userInput)
+
+
+elif userChoice.lower() == "d":
+    decrypt(userInput)
