4f1debcdfe0e29bf9f680ee6ded3cf22347c8ce9
Adham <iamnotadham@gmail.com>
2024-06-01 04:01:12 +0300

    Solving Errors

--- SoApp/signup.py
+++ SoApp/signup.py
@@ -1,22 +1,38 @@
-# import all requird lib & function
-
-# import ncrpt.py some function to make encrption
+# Import necessary libraries
 from ncrpt import *
-# import hashing.py one function to make hashing
 from hashing import *
-# import sqlite3 lib to make connection between sqlite Database and python app
 import sqlite3
 
-# connect to Database File
+# Connect to the database
 db = sqlite3.connect("user_info.db")
-
-# to set DataBase cursor in var
 cursor = db.cursor()
 
-# To make 2 table if not exists
-cursor.execute("CREATE TABLE IF NOT EXISTS signup_info (username TEXT NOT NULL, password TEXT, email TEXT NOT NULL, firstname TEXT, lastname TEXT, PRIMARY KEY(username, email));")
-cursor.execute("CREATE TABLE IF NOT EXISTS user_info (username TEXT NOT NULL, rounds INTEGER, lose rounds	INTEGER, win rounds	INTEGER, points	INTEGER, lose points INTEGER, win points INTEGER, PRIMARY KEY(username), FOREIGN KEY (username) REFERENCES signup_info (username));")
-# to defline some var
+# Create tables if not exists
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS signup_info (
+    username TEXT NOT NULL, 
+    password TEXT, 
+    email TEXT NOT NULL, 
+    firstname TEXT, 
+    lastname TEXT, 
+    PRIMARY KEY(username, email)
+);
+""")
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS user_info (
+    username TEXT NOT NULL, 
+    rounds INTEGER, 
+    lose_rounds INTEGER, 
+    win_rounds INTEGER, 
+    points INTEGER, 
+    lose_points INTEGER, 
+    win_points INTEGER, 
+    PRIMARY KEY(username), 
+    FOREIGN KEY (username) REFERENCES signup_info (username)
+);
+""")
+
+# Define variables
 fullName = ""
 firstName = ""
 userName = ""
@@ -28,110 +44,80 @@ rePassword = ""
 print("------------Sign up------------")
 print("  Please enter your information  ")
 
-def makelistofsignup_info():
+def dataBase():
     cursor.execute("SELECT username, password, email FROM signup_info")
     alli = cursor.fetchall()
-    linfo = []
-    fa = ""
+    userInfo = []
     for i in alli:
-        r = str(i[0])
-        l = str(i[1])
-        fa = r + " " + l
-        li = fa.split(" ")
-        linfo.append(li)
-    return linfo
+        userInfo.append(list(i))
+    return userInfo
 
-linfo = makelistofsignup_info()
+userInfo = dataBase()
 
-# Function to get user info to sign up with this info
+def ValidPass():
+    while True:
+        password = input("Enter your password (must be at least 8 characters long and include at least one number) \n --> ")
+        if len(password) < 8:
+            print("Password must be at least 8 characters long.")
+        elif not any(char.isdigit() for char in password):
+            print("Password must include at least one number.")
+        else:
+            rePassword = input("Re-enter your password for confirmation \n --> ")
+            if password == rePassword:
+                return password
+            else:
+                print("Passwords do not match. Please try again.")
+
 def signup():
-    global fullName, firstName, lastName, userEmail, password, rePassword, userName
+    global fullName, firstName, lastName, userEmail, password, userName
 
-    # check if user entered full name
     while True:
         fullName = input("Enter your full name \n --> ")
-        if " " in fullName:
-            # Check if first name true
-            if fullName[0] == " ":
-                print("You didn't your first name")
-            else:
-                firstName, lastName = fullName.split(" ", 1)
-                break
-
+        if " " in fullName and fullName[0] != " ":
+            firstName, lastName = fullName.split(" ", 1)
+            break
         else:
             print("Please enter your full name with a space between first and last names.")
-# the first in erorr
+
     while True:
         userName = input("Enter your username \n --> ")
-        username = forget(userName)
-        for i in linfo:
-            if username == i[0]:
-                print("now, This username was used to")
-            else:
-                print("")
-                print(i[0], username)
-                print("")
-                break
-# the end in erorr
+        userName = forget(userName)
+        if any(userName == user[0] for user in userInfo):
+            print("This username is already taken.")
+        else:
+            break
+
     while True:
         userEmail = input("Enter your email address \n --> ")
         if "@" in userEmail:
-            Email = forget(userEmail)
-            for t in linfo :
-                if Email == i[3]:
-                    print("now, email address was used to")
-                else :
-                    break
-        else:
-            print("That's not a valid email address")
-            print("----------------------")
-
-    def ValidPass():
-        while True:
-            password = input(
-                "Enter your password (must be at least 8 characters long and include at least one number) \n --> ")
-
-            # Check if password meets requirements
-            if len(password) < 8:
-                print("Password must be at least 8 characters long.")
-            elif not any(char.isdigit() for char in password):
-                print("Password must include at least one number.")
+            userEmail = forget(userEmail)
+            if any(userEmail == user[2] for user in userInfo):
+                print("This email address is already used.")
             else:
-                # Ask user to re-enter password for confirmation
-                rePassword = input("Re-enter your password for confirmation \n --> ")
-                if password == rePassword:
-                    return password
-                elif password != rePassword:
-                    print("Passwords do not match. Please try again.")
-                    ValidPass()
+                break
+        else:
+            print("That's not a valid email address.")
+            print("----------------------")
 
     password = ValidPass()
 
-# Run Main Function sign up
-
 signup()
 
-# encrption all user info
-
-# fullName = encrypt(fullName) Not important
+# Encrypt and hash user info
 firstName = encrypt(firstName)
 lastName = encrypt(lastName)
 password = encrypt(password)
-# Hashing user info
+
 try:
-    # Call the hashing function and store the hashed password
     password = HashingInfo(password)
     print(f"Successfully signed up")
 except Exception as e:
     print(f"An error occurred: {e}")
 
-# to add this info to database
-cursor.execute("SELECT username FROM signup_info")
-cursor.execute("")
-cursor.execute(f"INSERT INTO signup_info(username, password, email, firstname, lastname) VALUES(?, ?, ?, ?, ?)", (username, password, Email, firstName, lastName))
+# Insert user info into the database
+cursor.execute(f"INSERT INTO signup_info(username, password, email, firstname, lastname) VALUES(?, ?, ?, ?, ?)",
+               (userName, password, userEmail, firstName, lastName))
 
-# to save all changs in database
-
+# Save changes and close the database
 db.commit()
-
 db.close()
